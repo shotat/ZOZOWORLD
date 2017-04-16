8b78957afeb6dc71461d5d702c0c0137395188d4
shotat <shotat@users.noreply.github.com>
2017-04-16 14:25:12 +0900

    create goods

--- db/migrate/20170416052047_create_goods.rb
+++ db/migrate/20170416052047_create_goods.rb
@@ -1,9 +1,9 @@
 class CreateGoods < ActiveRecord::Migration[5.1]
   def change
     create_table :goods do |t|
-      t.string :name
-      t.integer :price
-      t.string :image_url
+      t.string  :name,      default: '', null: false
+      t.integer :price,     default: 0,  null: false
+      t.string  :image_url, default: '', null: false
 
       t.timestamps
     end
